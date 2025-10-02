8fe1cb62040db31f6b9c5790fac5b9260bb5589d
alexdelarge098 <prithyush.sethumadhavan@gmail.com>
2025-10-02 05:46:36 +0530

    Added additional array scripts to the conditional branch

---
+++ arrays3.sh
@@ -0,0 +1,3 @@
+read -p "enter the names" names
+
+echo "${names[0]} ${names[1]} ${names[2]}"
---
+++ arrays4.sh
@@ -0,0 +1,7 @@
+conditions=( "$BASH" "$USERNAME" "$SHELL" )
+
+echo "Print the output: ${conditions[0]}"
+
+echo "Print the output:${conditions[1]}"
+
+echo "Print the output:${conditions[2]}"
