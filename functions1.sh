b1c4ae9af93c71bbc5d61cbed27be426466489f3
alexdelarge098 <prithyush.sethumadhavan@gmail.com>
2025-11-12 09:59:35 +0530

    adding functions

---
+++ functions1.sh
@@ -0,0 +1,5 @@
+greet_user() {
+	echo "Welcome to the bash scripting"
+}
+
+greet_user 
---
+++ functions2.sh
@@ -0,0 +1,5 @@
+greet_with_age() {
+	echo " Hello $1!, you are $2 years old"
+}
+
+greet_with_age "Priset" 28
